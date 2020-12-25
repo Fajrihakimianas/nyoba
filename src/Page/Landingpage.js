2ef5f5d93c2189676bdff34777f3d94c5c391b12
Fajrihakimianas <fajri.hakimianas@gmail.com>
2020-12-25 10:54:33 +0700

    First Init

---
+++ src/Page/Landingpage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Landingpage extends Component {
+    render() {
+        return (
+            <div>
+                <div className="header">
+                    
+                </div>
+            </div>
+        )
+    }
+}
